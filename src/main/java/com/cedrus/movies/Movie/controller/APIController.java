6712ba6c184eaf9e26fce176dcbfd393bf1ead0a
John <john.sangiolo@cedrus.digital>
2019-04-23 10:57:58 -0400

    add endpoint to retrieve movies based on genre id from external api

--- src/main/java/com/cedrus/movies/Movie/controller/APIController.java
+++ src/main/java/com/cedrus/movies/Movie/controller/APIController.java
@@ -3,7 +3,9 @@ package com.cedrus.movies.Movie.controller;
 import com.cedrus.movies.Movie.model.internal.Categories;
 import com.cedrus.movies.Movie.service.APIService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +17,10 @@ public class APIController {
     @RequestMapping("/api")
     public Categories api() {
         return apiService.getCategories();
+    }
+
+    @RequestMapping(value = "api/genre/{id}", method = RequestMethod.GET)
+    public Object getMoviesByGenre(@PathVariable int id) {
+        return apiService.getMoviesByGenre(id);
     }
 }
--- src/main/java/com/cedrus/movies/Movie/service/APIService.java
+++ src/main/java/com/cedrus/movies/Movie/service/APIService.java
@@ -1,13 +1,13 @@
 package com.cedrus.movies.Movie.service;
 
 import com.cedrus.movies.Movie.model.internal.Categories;
+import com.cedrus.movies.Movie.model.internal.Config;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class APIService {
 
-//    @RequestMapping(value= "https://api.themoviedb.org/3/genre/movie/list?api_key=e0fef5117db0c8a76a608500a570fa26&language=en-US", method = RequestMethod.GET)
     public Categories getCategories() {
         RestTemplate restTemplate = new RestTemplate();
         String key = new Config().getApiKey();
@@ -16,4 +16,10 @@ public class APIService {
         return categories;
     }
 
+    public Object getMoviesByGenre(int genreId) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://api.themoviedb.org/3/discover/movie?api_key=e0fef5117db0c8a76a608500a570fa26&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=" + genreId;
+        Object movieResults = restTemplate.getForObject(url, Object.class);
+        return movieResults;
+    }
 }
